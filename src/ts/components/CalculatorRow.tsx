4dad79e1018e555d8c0ef30c2f91ee893713a518
nemanja-gajicic <nemanja.gajicic@gmail.com>
2022-11-25 01:10:46 +0100

    Basic styling

--- src/ts/components/CalculatorRow.tsx
+++ src/ts/components/CalculatorRow.tsx
@@ -1,7 +1,8 @@
 export const CalculatorRow = (props: any) => {
     return (
-        <div>
+        <div className={props.isDisabled ? "fitWidth disabled" : "fitWidth"}>
             <select
+                className="lrMargin"
                 value={props.sign}
                 disabled={props.isDisabled}
                 onChange={e => props.handleSelectChange(e, props.id)}>
@@ -9,18 +10,21 @@ export const CalculatorRow = (props: any) => {
                 <option value="plus">+</option>
             </select>
             <input type="number"
+                className="lrMargin"
                 value={props.value}
                 disabled={props.isDisabled}
                 onChange={e => props.handleInputChange(e, props.id)}
                 min="0"
-                tabIndex={props.id+1}
+                tabIndex={props.id + 1}
             />
             <button type="button"
+                className="lrMargin"
                 disabled={props.isDisabled}
                 onClick={() => props.handleDeleteClick(props.id)}>
                 Delete
             </button>
             <button type="button"
+                className="lrMargin"
                 onClick={() => props.handleDisableToggle(props.id)}>
                 {props.isDisabled ? "Enable" : "Disable"}
             </button>
